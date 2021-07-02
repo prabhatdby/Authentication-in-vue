b69f92c67af220a808fd44a56ebb460ec77e318b
prabhatdby <58321293+prabhatdby@users.noreply.github.com>
2021-07-03 00:59:08 +0530

    Create index.js

---
+++ src/router/index.js
@@ -0,0 +1,62 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import firebase from "firebase";
+
+const routes = [
+    {
+        path: "/",
+        name: "Home",
+        component: Home
+    },
+    {
+        path: "/login",
+        name: "Login", 
+        component: () => import("../views/Login.vue")
+    },
+    {
+        path: "/signup",
+        name: "Signup", 
+        component: () => import("../views/Signup.vue")
+    },
+    {
+        path: "/forgotpassword",
+        name: "ForgotPassword",
+        component: () => import("../views/ForgotPassword.vue"),
+        beforeEnter(to, from, next) {
+          const user = firebase.auth().currentUser;
+          if (user) {
+            next({ name: "dashboard" });
+          } else {
+            next();
+          }
+        },
+      },
+    {
+        path: "/dashboard",
+        name: "Dashboard", 
+        component: () => import("../views/Dashboard.vue"),
+        meta: {
+            requiresAuth: true
+        }
+    }
+];
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+});
+
+router.beforeEach((to, from, next) => {
+    const authenticatedUser = firebase.auth().currentUser;
+    const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+    
+    if ( requiresAuth && ! authenticatedUser ) {
+        alert("You are not authorized to access this area.");
+        next("login");
+    }
+    else {
+        next();
+    }
+});
+
+export default router;
